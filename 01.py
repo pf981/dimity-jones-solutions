eafb409385f6b989275f0ee6ca536ceda371b3ab
pf981 <paulfstr@gmail.com>
2025-03-20 00:31:32 +1100

    Fix missing last character in 01

--- 01.py
+++ 01.py
@@ -5,6 +5,7 @@ text = text.split("#####", 1)[1]
 output = []
 for i in range(len(text) // 2):
     output.append(text[i] + text[-i - 1])
+output.append(text[len(text) // 2])
 
 with open('./data/01.txt', 'w') as f:
     f.write(''.join(output))
