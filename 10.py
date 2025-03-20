603a832e86940ed1b516ef16f75af64714a096a2
Paul Foster <paulfstr@gmail.com>
2025-03-20 23:57:25 +1100

    Simplify alphabetical transposition key

--- 10.py
+++ 10.py
@@ -17,57 +17,22 @@ def chunk_shuffle(cipher: str, sequence: list[int], chunk_size: int | None = Non
     return "".join(result)
 
 
-line = "These make up words like feathers make birds."
-chars = [c.upper() for c in line if c.isalpha()]
-ranks = sorted(enumerate(chars), key=lambda pair: pair[1])
-score = [0] * len(chars)
-for i, (j, _) in enumerate(ranks, 1):
-    score[j] = i
-score
+def get_alphabetical_transposition_key(s: str):
+    chars = [c.upper() for c in s if c.isalpha()]
+    ranks = sorted(enumerate(chars), key=lambda pair: pair[1])
+
+    key = [0] * len(chars)
+    for i, (j, _) in enumerate(ranks, 1):
+        key[j] = i
+
+    return key
 
 
 @decrypter.decrypter(chapter=10)
 def decrypt(cipher: str) -> str:
     return chunk_shuffle(
         cipher,
-        score,
-        # [
-        #     34,
-        #     15,
-        #     7,
-        #     30,
-        #     8,
-        #     23,
-        #     1,
-        #     19,
-        #     9,
-        #     36,
-        #     26,
-        #     37,
-        #     25,
-        #     27,
-        #     5,
-        #     31,
-        #     22,
-        #     17,
-        #     20,
-        #     10,
-        #     14,
-        #     11,
-        #     2,
-        #     35,
-        #     16,
-        #     12,
-        #     28,
-        #     32,
-        #     24,
-        #     3,
-        #     21,
-        #     13,
-        #     4,
-        #     18,
-        #     29,
-        #     6,
-        #     33,
-        # ],
+        get_alphabetical_transposition_key(
+            "These make up words like feathers make birds."
+        ),
     )
