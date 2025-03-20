92fb8e0e788c3cfa9f01b40d5b118da791958b8a
Paul Foster <paulfstr@gmail.com>
2025-03-20 16:45:32 +1100

    Handle chapter 0 in decrypter

--- decrypter/__init__.py
+++ decrypter/__init__.py
@@ -6,20 +6,23 @@ class Decrypter:
     def __init__(self, func: Callable[[str], str], chapter: int, path: str):
         self.func = func
         self.chapter = chapter
+        self.prev_chapter = max(chapter - 1, 0)
         self.path = Path(path)
 
     def __call__(self, cipher: str) -> str:
         return self._decrypt(cipher)
 
     def decrypt_chapter(self) -> str:
-        cipher = self._read_file(f"{self.chapter:02}.txt").split("#####", 1)[1]
+        cipher = self._read_file(f"{self.prev_chapter:02}.txt")
+        if self.chapter:
+            cipher = cipher.split("#####", 1)[1]
         return self._decrypt(cipher)
 
     def decrypt_one_chapter(self) -> str:
         return self.decrypt_chapter().split("#####", 1)[0]
 
     def decrypt_example(self, example: int) -> str:
-        cipher = self._read_file(f"{self.chapter:02}.ex{example}")
+        cipher = self._read_file(f"{self.prev_chapter:02}.ex{example}")
         return self._decrypt(cipher)
 
     def _decrypt(self, cipher: str) -> str:
