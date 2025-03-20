97984bf26d342eb8c1b15cdfe41d961cdce0622f
Paul Foster <paulfstr@gmail.com>
2025-03-20 18:13:12 +1100

    Fix 01 adding extra character

--- 01.py
+++ 01.py
@@ -5,7 +5,9 @@ import decrypter
 def decrypt(cipher: str) -> str:
     output = []
 
-    for i in range(len(cipher) // 2 + len(cipher) % 2):
+    for i in range(len(cipher) // 2):
         output.append(cipher[i] + cipher[-i - 1])
+    if len(cipher) % 2:
+        output.append(cipher[len(cipher) // 2])
 
     return "".join(output)
