b7aa06b98ac353ac5f2bf7ff94461ae7bf91b79d
pf981 <paulfstr@gmail.com>
2025-06-17 19:50:42 +1000

    Simplify solution

--- solutions/62.py
+++ solutions/62.py
@@ -1,15 +1,31 @@
+import collections
+import string
+
 import decrypter
+
+text = """
+Dear Hugh,
+
+At midnight, I have been trying to fix things in my mind.
+
+Counting up things ... my excuses, my sins, my shortsightednesses, my anxieties, my wounds, my anguish, my former mistakes ... Counting up exactly my many EX-SLIP-UPs.
+
+Hoping you're not unable to forgive
+your
+Suzi
+"""
+
+counts = collections.Counter(text.lower())
+result = "".join(string.ascii_lowercase[counts[ch] - 1] for ch in "exslipup")
+print(result)  # rescueme
 
 
 @decrypter.decrypter(chapter=62)
 def decrypt(cipher: str) -> str:
-    return decrypter.vigenere_cipher(
-        cipher, "rescue me"
-    )
+    return decrypter.vigenere_cipher(cipher, "rescue me")
+
 
 # 'reocue mlreslufcmereubyemmertscuednu'
-
-
 
 
 # import decrypter
@@ -82,7 +98,6 @@ def decrypt(cipher: str) -> str:
 #         distance += abs(freq1 - freq2)
 
 #     return distance
-
 
 
 # def get_normalised_three_grams(text: str) -> collections.Counter:
