5fd0d7e7a8aa3fc8ffdfcded91fe652b71a9ea32
Paul Foster <paulfstr@gmail.com>
2026-01-03 14:17:31 +1100

    Tidy chapter 89

--- solutions/89.py
+++ solutions/89.py
@@ -4,23 +4,21 @@ import decrypter
 ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,?!:;'\"-()[]{}|+=%/\\*#$_ \n"
 ALPHABET_IDX = {c: i for i, c in enumerate(ALPHABET)}
 
-ciphertext = "\n".join(final_tapestry_lines)
 key = "DIsENCuMBERED"
 
 
-def decrypt_additive(ciphertext: str, key: str) -> str:
+def decrypt_additive(cipher: str, key: str) -> str:
     result = []
     i = 0
-    for c in ciphertext:
+    for c in cipher:
         if c == "\n":
-            # result.append("\n")
-            pass
-        else:
-            c_idx = ALPHABET_IDX[c]
-            k_idx = ALPHABET_IDX[key[i % len(key)]]
-            p_idx = (c_idx - k_idx) % 89
-            result.append(ALPHABET[p_idx])
-            i += 1
+            continue
+
+        c_idx = ALPHABET_IDX[c]
+        k_idx = ALPHABET_IDX[key[i % len(key)]]
+        p_idx = (c_idx - k_idx) % 89
+        result.append(ALPHABET[p_idx])
+        i += 1
     return "".join(result)
 
 
